f8ae11dcfb8b1fdac232e68f470b305184fc044d
nascal3 <nascal3@gmail.com>
2019-05-20 12:38:31 +0300

    actual changes to session check error fix

--- components/session-check-index.php
+++ components/session-check-index.php
@@ -1,8 +1,8 @@
 <?php
-    if($_SESSION['usertype']=='tenant'){
-        header("location:tenant_profile.php");
-    }
-	 if($_SESSION['usertype']=='landlord'){
-        header("location:landlord_profile.php");
-    }
+if(isset($_SESSION['usertype']) && $_SESSION['usertype']=='tenant'){
+    header("location:tenant_profile.php");
+}
+if(isset($_SESSION['usertype']) && $_SESSION['usertype']=='landlord'){
+    header("location:landlord_profile.php");
+}
 ?>
