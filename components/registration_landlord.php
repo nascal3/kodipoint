9614006be4220a7efd4c2e44d1f5c290f7884a9c
nascal3 <nascal3@gmail.com>
2019-05-21 12:43:42 +0300

    fix how userId is assigned

--- components/registration_landlord.php
+++ components/registration_landlord.php
@@ -8,14 +8,16 @@
         $user_tel=$_REQUEST['user_tel'];
         $user_password=$_REQUEST['user_password'];
 		$user_password= base64_encode(hash('sha256',$user_password));
-        $sql="INSERT INTO re_landlords(firstname,lastname,email,telephone) VALUES('$user_firstname','$user_lastname','$user_email','$user_tel')";
+
+        $sql="INSERT INTO re_users(user_email,user_password) VALUES('$user_email','$user_password')";
         mysqli_query($conn,$sql) or die(mysqli_error($conn));
-		$sql2="SELECT id FROM re_landlords WHERE email = '$user_email'";
+
+		$sql2="SELECT id FROM re_users WHERE user_email = '$user_email'";
 		$result= mysqli_query($conn,$sql2) or die(mysqli_error($conn));
 		$row = mysqli_fetch_array($result);
 		$landlordid = $row[0];
-		
-		$sql3="INSERT INTO re_users(user_id,user_email,user_password,user_type) VALUES('$landlordid','$user_email','$user_password','landlord')";
+
+        $sql3="INSERT INTO re_landlords(user_id,firstname,lastname,email,telephone) VALUES('$landlordid','$user_firstname','$user_lastname','$user_email','$user_tel')";
 		mysqli_query($conn,$sql3) or die(mysqli_error($conn));
 		
         $_SESSION['userid'] = $landlordid;
