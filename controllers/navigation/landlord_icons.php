383ff5f8e04f42acc197ceeca78afde1956cee4a
nascal3 <nascal3@gmail.com>
2019-05-22 14:11:11 +0300

    add relevant icons and link to page

--- controllers/navigation/landlord_icons.php
+++ controllers/navigation/landlord_icons.php
@@ -4,8 +4,8 @@
 			<div class="col-md-2"><a href="property.php"><div class="iconbox"><i class="fa fa-building-o" aria-hidden="true"></i>&nbsp;&nbsp;Properties</div></a></div>
 			<div class="col-md-2"><a href="tenants.php"><div class="iconbox"><i class="fa fa-address-book-o" aria-hidden="true"></i>&nbsp;&nbsp;Tenants</div></a></div>
 			<div class="col-md-2"><a href="invoicing.php"><div class="iconbox"><i class="fa fa-files-o" aria-hidden="true"></i>&nbsp;&nbsp;Invoicing</div></a></div>
-			<div class="col-md-2"><a href="payments.php"><div class="iconbox"><i class="fa fa-money" aria-hidden="true"></i>&nbsp;&nbsp;Payments</div></a></div>
-			<div class="col-md-2"><a href="records.php"><div class="iconbox"><i class="fa fa-book" aria-hidden="true"></i>&nbsp;&nbsp;Records</div></a></div>
+			<div class="col-md-2"><a href="payments.php"><div class="iconbox"><i class="fa fa-money" aria-hidden="true"></i>&nbsp;&nbsp;Transactions</div></a></div>
+			<div class="col-md-2"><a href="webpay/index.php"><div class="iconbox"><i class="fa fa-google-wallet" aria-hidden="true"></i>&nbsp;&nbsp;Payments</div></a></div>
 			<div class="col-md-2"><a href="stats.php"><div class="iconbox"><i class="fa fa-pie-chart" aria-hidden="true"></i>&nbsp;&nbsp;Stats</div></a></div>
 		</div>
 	</article>
