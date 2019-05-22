19e05172fcbaae7ba6d44664fa06eb02526fb689
nascal3 <nascal3@gmail.com>
2019-05-22 16:52:33 +0300

    fixed registration of tenant to have tenant role number

--- components/registration_tenant.php
+++ components/registration_tenant.php
@@ -7,19 +7,19 @@
         $user_password=$_REQUEST['user_password'];
 		$user_password= base64_encode(hash('sha256',$user_password));
 
-		$sql="UPDATE re_tenant SET telephone='$user_tel' WHERE email='$user_email'";
-        mysqli_query($conn,$sql) or die(mysqli_error($conn));
+        $sql3="INSERT INTO re_users(user_email,user_password,role) VALUES('$user_email','$user_password',1)";
+        mysqli_query($conn,$sql3) or die(mysqli_error($conn));
 		
-		$sql2="SELECT id FROM re_tenant WHERE email = '$user_email'";
+		$sql2="SELECT id FROM re_users WHERE user_email = '$user_email'";
 		$result= mysqli_query($conn,$sql2) or die(mysqli_error($conn));
 		$row = mysqli_fetch_array($result);
-		$tenantid = $row[0];
-		
-		$sql3="INSERT INTO re_users(user_id,user_email,user_password,user_type) VALUES('$tenantid','$user_email','$user_password','tenant')";
-		mysqli_query($conn,$sql3) or die(mysqli_error($conn));
+		$user_id = $row[0];
+
+        $sql="UPDATE re_tenant SET telephone='$user_tel', user_id='$user_id' WHERE email='$user_email'";
+        mysqli_query($conn,$sql) or die(mysqli_error($conn));
         
-        $_SESSION['userid'] = $tenantid;
-        $_SESSION['usertype'] = "tenant";
+        $_SESSION['userid'] = $user_id;
+        $_SESSION['usertype'] = 1;
         header('Location: ../tenant_profile.php');
     }
 ?>
