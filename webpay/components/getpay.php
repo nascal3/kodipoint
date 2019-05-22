c7afb482ddc60bd2c5129d6356885ed0d11c8e4a
nascal3 <nascal3@gmail.com>
2019-05-22 15:37:30 +0300

    fixed payments method saving

--- webpay/components/getpay.php
+++ webpay/components/getpay.php
@@ -3,13 +3,18 @@
 	
 	if(!empty($_POST)){
 	
-	$cname=$_REQUEST['customerName'];
+	$cnumber=$_REQUEST['customerName'];
 	$mnumber=$_REQUEST['mobileNumber'];
 	$amount=$_REQUEST['amount'];
 	$transref=$_REQUEST['transactionRefNo'];
 	$datetime=$_REQUEST['timeStamp'];
+
+        $sqlx = "SELECT name FROM re_tenant WHERE id = '$cnumber'";
+        $resultx = mysqli_query($conn,$sqlx) or die(mysqli_error());
+        $rowx = mysqli_fetch_array($resultx);
+        $cname = $rowx['name'];
 	
-	$sql="INSERT INTO re_payment(trans_identify,trans_time,trans_amount,trans_tel,trans_customername,trans_status) VALUES('$transref','$datetime','$amount','$mnumber','$cname', '1')";
+	    $sql="INSERT INTO re_payment(trans_identify,trans_time,trans_amount,trans_tel,tenant_id,trans_status,trans_customername) VALUES('$transref','$datetime','$amount','$mnumber','$cnumber', '1','$cname')";
 	
 	mysqli_query($conn,$sql) or die(mysqli_error($conn));
 	header('Location: ../index.php?status=success');
