30ccc8b8ab6d62641cd979e632ec792d32065522
nascal3 <nascal3@gmail.com>
2019-05-22 16:24:06 +0300

    fixed session user type is not landlord

--- webpay/components/session-check.php
+++ webpay/components/session-check.php
@@ -3,7 +3,7 @@
         header("location:login.php?session=notset");
     }
     $current_user_type = $_SESSION['usertype'];
-    if($current_user_type == 1){
+    if($current_user_type != 2){
         header("location:login.php?session=notset");
     }
 ?>
