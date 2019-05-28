c6ec15f0560c51da5fe63d3bae22ff59a6237303
nascal3 <nascal3@gmail.com>
2019-05-28 15:42:48 +0300

    fixed fetch payments records to get only landlord specific records and indented code

--- webpay/components/getpay.php
+++ webpay/components/getpay.php
@@ -3,21 +3,22 @@
 	
 	if(!empty($_POST)){
 	
-	$cnumber=$_REQUEST['customerName'];
-	$mnumber=$_REQUEST['mobileNumber'];
-	$amount=$_REQUEST['amount'];
-	$transref=$_REQUEST['transactionRefNo'];
-	$datetime=$_REQUEST['timeStamp'];
+        $cnumber=$_REQUEST['customerName'];
+        $mnumber=$_REQUEST['mobileNumber'];
+        $amount=$_REQUEST['amount'];
+        $transref=$_REQUEST['transactionRefNo'];
+        $datetime=$_REQUEST['timeStamp'];
+        $landlord_id=$_REQUEST['landlord_id'];
 
         $sqlx = "SELECT name FROM re_tenant WHERE id = '$cnumber'";
         $resultx = mysqli_query($conn,$sqlx) or die(mysqli_error());
         $rowx = mysqli_fetch_array($resultx);
         $cname = $rowx['name'];
 	
-	    $sql="INSERT INTO re_payment(trans_identify,trans_time,trans_amount,trans_tel,tenant_id,trans_status,trans_customername) VALUES('$transref','$datetime','$amount','$mnumber','$cnumber', '1','$cname')";
+	    $sql="INSERT INTO re_payment(trans_identify,trans_time,trans_amount,trans_tel,tenant_id,trans_status,trans_customername,landlord_id) VALUES('$transref','$datetime','$amount','$mnumber','$cnumber', '1','$cname','$landlord_id')";
 	
-	mysqli_query($conn,$sql) or die(mysqli_error($conn));
-	header('Location: ../index.php?status=success');
+        mysqli_query($conn,$sql) or die(mysqli_error($conn));
+        header('Location: ../index.php?status=success');
 	
 	}else{
 		echo "error#1245"; 
